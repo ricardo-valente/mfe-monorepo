eab99abafdb88efe71a4b833c0f66d7852f948ff
Richardo Powerdot <rva@powerdot.pt>
2023-08-09 17:40:56 +0100

    feat: add app two

---
+++ apps/app-two/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+
+import Content from "about/Content";
+
+export default function App() {
+  return (
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Content title="Solutions app" />
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("app")!);
+root.render(<App />);
